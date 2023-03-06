36e132e409f94c313ed7316a13d855debbea7478
kejun <kejun1997@gmail.com>
2023-03-06 16:09:44 +0800

    feat: add coding function tool

--- data/tools.ts
+++ data/tools.ts
@@ -40,6 +40,31 @@ const tools: Tool[] = [
     },
   },
   translator,
+  {
+    title: "写函数",
+    desc: "函数专家",
+    name: "coding_function",
+    icon: "i-mdi:code-braces-box",
+    systemMessageTemplate:
+      "你现在是一个 ${language} 专家, 请帮我用 ${language} 写一个函数，它需要实现以下功能",
+    userMessageTemplate: "${message}",
+    forms: {
+      language: {
+        type: "AInput",
+        props: {
+          placeholder: "比如：JavaScript，GO",
+          label: "语言",
+        },
+        default: "JavaScript",
+      },
+      message: {
+        type: "ATextarea",
+        props: {
+          placeholder: "比如：对象深拷贝",
+        },
+      },
+    },
+  },
 ];
 
 export default tools;
