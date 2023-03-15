03b7f2f7073a594b85dc3a32f86e332489a5bb76
kejun <kejun1997@gmail.com>
2023-03-15 11:17:01 +0800

    feat: add all gpt model

--- components/app/AppGPTSettingForms.vue
+++ components/app/AppGPTSettingForms.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
 const { storageOptions: options } = useChatGPT();
+const models = [
+  "gpt-4",
+  "gpt-4-0314",
+  "gpt-4-32k",
+  "gpt-4-32k-0314",
+  "gpt-3.5-turbo",
+  "gpt-3.5-turbo-0301",
+];
 </script>
 
 <template>
   <el-form size="large" label-position="top">
     <el-form-item label="API Key">
-      <el-input v-model="options.apiKey" placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"></el-input>
+      <el-input
+        v-model="options.apiKey"
+        placeholder="sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+      ></el-input>
     </el-form-item>
     <el-form-item label="API Base Url">
       <el-input
@@ -15,8 +26,7 @@ const { storageOptions: options } = useChatGPT();
     </el-form-item>
     <el-form-item label="Model">
       <el-select w-full v-model="options.model">
-        <el-option value="gpt-3.5-turbo-0301"></el-option>
-        <el-option value="gpt-3.5-turbo"></el-option>
+        <el-option v-for="item in models" :value="item"></el-option>
       </el-select>
     </el-form-item>
   </el-form>
