b1872687513e4ba716e26da9c5e87123691f1f8a
kejun <kejun1997@gmail.com>
2023-03-16 00:43:16 +0800

    feat: add pagination for whokshop

--- components/workshop/ToolList.vue
+++ components/workshop/ToolList.vue
@@ -1,43 +1,63 @@
 <script lang="ts" setup>
 const localePath = useLocalePath();
 const { search } = useQuerySearch();
-const { data: tools, pending } = useAsyncData(
+const pageSize = ref(9);
+const currentPage = ref(1);
+const { data, pending } = useAsyncData(
   () =>
     $fetch("/api/tool/all", {
       params: {
-        pageSize: 10,
-        currentPage: 0,
+        pageSize: pageSize.value,
+        currentPage: currentPage.value,
         keyword: search.value,
       },
     }),
   {
-    watch: [search],
+    watch: [search, pageSize, currentPage],
   }
 );
+const total = computed(() => data.value?.total ?? 0);
+const list = computed(() => data.value?.list ?? []);
 </script>
 
 <template>
   <div v-loading="pending">
-    <div
-      mt-8
-      grid
-      grid-cols-1
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-      gap-6
-    >
-      <NuxtLink
-        v-for="item in tools"
-        :key="item.id"
-        :to="localePath(`/ai-${item.id}`)"
+    <div min-h-120>
+      <div
+        mt-8
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        gap-6
       >
-        <ToolItem h-full :tool="item" />
-      </NuxtLink>
+        <NuxtLink
+          v-for="item in list"
+          :key="item.id"
+          :to="localePath(`/ai-${item.id}`)"
+        >
+          <ToolItem h-full :tool="item" />
+        </NuxtLink>
+      </div>
     </div>
+
     <el-empty
-      v-if="!tools?.length"
-      :description="`No '${search}' results found`"
+      v-if="!list?.length"
+      :description="
+        search === ''
+          ? 'There is nothing here :)'
+          : `No '${search}' results found`
+      "
     ></el-empty>
+    <el-pagination
+      hide-on-single-page
+      justify-center
+      background
+      layout="prev, pager, next"
+      v-model:page-size="pageSize"
+      v-model:current-page="currentPage"
+      :total="total"
+    />
   </div>
 </template>
--- server/api/tool/all.get.ts
+++ server/api/tool/all.get.ts
@@ -2,24 +2,38 @@ import { prisma } from "~/server/plugins/prisma";
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event);
-  const currentPage = parseInt((query.currentPage as string) ?? 0);
+  const currentPage = parseInt((query.currentPage as string) ?? 1);
   const pageSize = parseInt((query.pageSize as string) ?? 10);
   const keyword = (query.keyword as string) ?? "";
-  return await prisma.tool.findMany({
-    skip: currentPage * pageSize,
-    take: pageSize,
-    where: {
-      name: {
-        startsWith: keyword,
-        mode: "insensitive",
+  const result = await prisma.$transaction([
+    prisma.tool.count({
+      where: {
+        name: {
+          startsWith: keyword,
+          mode: "insensitive",
+        },
       },
-    },
-    select: {
-      icon: true,
-      name: true,
-      desc: true,
-      id: true,
-      author: true,
-    },
-  });
+    }),
+    prisma.tool.findMany({
+      skip: (currentPage - 1) * pageSize,
+      take: pageSize,
+      where: {
+        name: {
+          startsWith: keyword,
+          mode: "insensitive",
+        },
+      },
+      select: {
+        icon: true,
+        name: true,
+        desc: true,
+        id: true,
+        author: true,
+      },
+    }),
+  ]);
+  return {
+    total: result[0] ?? 0,
+    list: result[1],
+  };
 });
