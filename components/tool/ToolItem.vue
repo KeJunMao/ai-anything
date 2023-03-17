3afed232828033d2e308dd9da413e8e4b046d8a5
kejun <kejun1997@gmail.com>
2023-03-17 22:02:50 +0800

    chore: add desc to fixed max 2 line

--- components/tool/ToolItem.vue
+++ components/tool/ToolItem.vue
@@ -38,7 +38,14 @@ defineProps<{
     <div text-2xl font-semibold text-gray-900 dark:text-gray-100>
       {{ tool?.name }}
     </div>
-    <div v-if="tool?.desc" mt-1 font-medium text-gray-500 dark:text-gray-400>
+    <div
+      v-if="tool?.desc"
+      mt-1
+      font-medium
+      text-gray-500
+      dark:text-gray-400
+      sm:line-clamp-2
+    >
       {{ tool?.desc }}
     </div>
   </div>
