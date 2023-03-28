b7ba97ca259803320287d843ad499f3178006726
kejun <kejun1997@gmail.com>
2023-03-28 16:52:42 +0800

    fix: update pwa fallback deny list

--- config/pwa.ts
+++ config/pwa.ts
@@ -33,6 +33,7 @@ export const pwa: ModuleOptions = {
   },
   workbox: {
     globPatterns: ["**/*.{js,css,html,txt,png,ico,svg}"],
+    navigateFallbackDenylist: [/^\/api\//],
     navigateFallback: "/",
     cleanupOutdatedCaches: true,
     runtimeCaching: [
